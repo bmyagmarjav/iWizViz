a0c3dbccf712ee9f25709dccd55ee26f56800786
Battulga Myagmarjav <Tulga@Battulgas-MacBook-Pro.local>
2016-11-08 14:18:54 -0500

    removed test code and will be implemented properly in the future

--- src/app/components/sidebar/isidebar.js
+++ src/app/components/sidebar/isidebar.js
@@ -16,44 +16,7 @@
   });
 
   // contoller for sampel componenet
-  function isidebarController($log, $window, $firebaseObject, Firebaseio, D3srv) {
-    var self = this;
-    // download the data into a local object
-    var obj = $firebaseObject(Firebaseio.getMigrationRef());
-    // syncObject.$bindTo(self, 'username');
-    // self.username = obj;
-    obj.$loaded()
-    .then(function (data) {
-      self.username = data['2000']['From Midwest'].Northeast;
-    })
-    .catch(function (error) {
-      $log.info("Error:", error);
-    });
-    $log.info(obj);
+  function isidebarController() {
 
-    var sampleSVG = D3srv.select("#viz")
-    .append("svg")
-    .attr("width", 100)
-    .attr("height", 100);
-
-    sampleSVG.append("circle")
-    .style("stroke", "gray")
-    .style("fill", "white")
-    .attr("r", 40)
-    .attr("cx", 50)
-    .attr("cy", 50)
-    .on("mouseover", function () {
-      d3.select(this).style("fill", "aliceblue");
-    })
-    .on("mouseout", function () {
-      d3.select(this).style("fill", "white");
-    });
-
-    $log.info($window.innerWidth);
-
-    angular.element($window).bind('resize', function () {
-      $log.info($window.innerWidth);
-      $log.info($window.innerHeight);
-    });
   }
 })();
