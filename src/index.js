c2d5a9e896a2bb4fdd814aad074f03d13bd5978b
Battulga Myagmarjav <Tulga@Battulgas-MacBook-Pro.local>
2016-11-11 22:05:37 -0500

    container service on

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 (function () {
   'use strict';
-  // firebase service
   angular.module('app', []).service('Firebaseio', Firebaseio);
-  // d3js service
   angular.module('app').service('D3srv', D3srv);
+  angular.module('app').service('ContainerSrv', ContainerSrv);
 })();
