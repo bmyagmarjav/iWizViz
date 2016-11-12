8e61edcc672cd7935f41acc9492eef61b9b537c3
Battulga Myagmarjav <Tulga@Battulgas-MacBook-Pro.local>
2016-11-11 22:06:50 -0500

    radio inputs with labels - [years]

--- src/app/components/iradioslider/iradioslider.js
+++ src/app/components/iradioslider/iradioslider.js
@@ -7,7 +7,20 @@
     controller: iradiosliderController
   });
 
-  function iradiosliderController() {
+  function iradiosliderController($scope, ContainerSrv, $log) {
+    $scope.years = generateYears(2000, 2016);
+    $scope.selected = {year: '2001'};
+    $scope.$watch('selected.year', function (value) {
+      ContainerSrv.sharedYaar = value;
+      // $log.info(ContainerSrv.sharedYaar);
+    });
+  }
 
+  function generateYears(start, end) {
+    var years = [];
+    for (var year = start; year < end; year++) {
+      years.push(year);
+    }
+    return years;
   }
 })();
